574f5c3f45eea0de2ab5172fe3e41d7173377bac
Felipe Miguel <felipe.miguel.santos@hotmail.com>
2020-10-03 00:33:12 -0300

    OK

--- src/routes/notificacoes.mercadolivre.route.js
+++ src/routes/notificacoes.mercadolivre.route.js
@@ -58,7 +58,6 @@ module.exports = (io) => {
             if (req.body.topic === 'messages') {
                 await axios.get(`https://api.mercadolibre.com/messages/${req.body.resource}?access_token=${user.accessToken}`).then(async message => {
                     console.log(req.body)
-                    console.log(message.data)
                     io.emit("mensagem_pos_venda", message.data)
                     res.status(200).send(message.data)
                 })
