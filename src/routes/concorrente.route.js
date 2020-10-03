d776a8f823103013e9bf133a88a98ccc8d287e3b
Felipe Miguel <felipe.miguel.santos@hotmail.com>
2020-10-02 23:01:56 -0300

    OK

--- src/routes/concorrente.route.js
+++ src/routes/concorrente.route.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const concorrenteService = require("../../services/Concorrente/concorrente-service")
+const concorrenteService = require("../../services/concorrente-service")
 
 router.post('/', concorrenteService.getConcorrente) 
 router.post('/save/', concorrenteService.saveConcorrente)
