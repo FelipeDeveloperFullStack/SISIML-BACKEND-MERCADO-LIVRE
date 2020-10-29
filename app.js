2e2f366f5d6d317bf90c77cc3f6bb18da279d7fd
Felipe Miguel <felipe.miguel.santos@hotmail.com>
2020-10-28 23:52:18 -0300

    OK

--- app.js
+++ app.js
@@ -37,9 +37,23 @@ app.use(session({
 
 app.use(flash());
 app.use(cors());
+app.options('*', cors())
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
+
+/*app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://sisimlserver.herokuapp.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });*/
+  app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    next();
+});
 
 app.use('/anuncio', anuncioRoute);
 app.use('/', mercadoLivreRoute);
